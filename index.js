32b3c76bc02fb3ef1b075939d82ab4d3412153b9
davidpaulmcintyre <dmcintyre@tandemseven.com>
2015-06-05 14:25:44 -0400

    no msg nec

--- index.js
+++ index.js
@@ -2,5 +2,25 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-content-for-demo'
+  name: 'ember-cli-content-for-demo',
+
+  contentFor: function(type, config){
+    if (type === 'head'){
+      console.log('CONFIG = ' + config);
+      return '<link href="http://cdnjs.head.css" rel="stylesheet" type="text/css" />';
+
+    } else if (type === 'head-footer'){
+
+      return '<link href="http://cdnjs.head-footer.css" rel="stylesheet" type="text/css" />';
+
+    } else if (type === 'body'){
+
+      return '<div>this is body content-for content';
+
+    } else if (type === 'body-footer'){
+
+      return '<span>footer content (such as copyright, etc) goes here</span>';
+
+    }
+  }
 };
